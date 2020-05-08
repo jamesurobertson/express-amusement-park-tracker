6fd8dfc612b71f393f9a840316605ed9c3e44fc5
James Robertson <jamesurobertson@gmail.com>
2020-05-08 11:11:00 -0500

    initialized custom error handling

--- app.js
+++ app.js
@@ -10,4 +10,43 @@ app.set('view engine', 'pug')
 app.use(morgan('dev'))
 app.use(routes)
 
+// catch unhandled request
+app.use((req, res, next) => {
+  const err = new Error('The requested page could not be found.')
+  err.status = 404
+  next(err)
+})
+
+// log error to database or print to console
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    // Log error to databse (not set up yet.)
+  } else {
+    console.error(err)
+  }
+
+  next(err)
+})
+
+// display page-not-found page for 404's
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404)
+    res.render('page-not-found', {title: 'Page Not Found'})
+  } else {
+    next(err)
+  }
+})
+
+//generic Server error
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  const isProduction = process.env.NODE_ENV === 'production'
+  res.render('error', {
+    title: 'Server Error',
+    message: isProduction ? null: err.message,
+    stack: isProduction ? null : err.stack
+  })
+})
+
 module.exports = app
--- routes.js
+++ routes.js
@@ -7,4 +7,10 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Home' })
 })
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/error-test', (req, res) => {
+    throw new Error('This is a test error')
+  })
+}
+
 module.exports = router
