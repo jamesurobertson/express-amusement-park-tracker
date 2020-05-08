342b8807f584b6cf4d9b8ca1fd35d3e1190e706d
James Robertson <jamesurobertson@gmail.com>
2020-05-08 17:53:34 -0500

    added Add Park page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 const routes = require('./routes')
 const { environment } = require('./config/index')
@@ -9,6 +10,8 @@ const app = express();
 app.set('view engine', 'pug')
 
 app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
 app.use(routes)
 
 // catch unhandled request
